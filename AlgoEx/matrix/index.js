0d8244dd074cad9cbd982e4c0174cc07a9cac869
vaz007 <ankur@adaptideations.com>
2020-03-29 00:54:17 +1100

    Matrix

--- AlgoEx/matrix/index.js
+++ AlgoEx/matrix/index.js
@@ -15,6 +15,54 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  const results = [];
+  for (let i = 0; i < n; i++) {
+    results.push([]);
+  }
+  let counter = 1;
+  let startCol = 0;
+  let endCol = n - 1;
+  let startRow = 0;
+  let endRow = n - 1;
 
+  while (startCol <= endCol && startRow <= endRow) {
+      
+      // Top row
+
+      for (let i = startCol; i <= endCol; i++){
+          results[startRow][i] = counter
+          counter++
+      }
+      startRow++
+
+      // Right Column
+
+      for (let i =  startRow; i <= endRow; i++){
+          results[i][endCol] = counter
+          counter++
+      }
+      endCol--
+
+      //Bottom Row
+
+      for (let i = endCol; i>= startCol; i--){
+          results[endRow][i] = counter++
+      }
+      endRow--
+      
+
+      //Start Column
+      for (let i = endRow; i>= startRow; i--){
+        results[i][startCol] = counter
+        counter++
+    }
+    startCol++
+    
+    // console.log(results)
+  }
+  return results
+}
+
+matrix(3);
 module.exports = matrix;
--- AlgoEx/vowels/index.js
+++ AlgoEx/vowels/index.js
@@ -7,6 +7,19 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
-
+function vowels(str) {
+    str = str.toLowerCase()
+    let count =0
+    const vowels = ['a', 'e', "i", "o", "u"];
+    for (let i=0; i < str.length; i++){
+        vowels.map(v => {
+            if (v === str[i]){
+              count +=1
+            }
+        })
+    }
+    console.log(count)
+    return count
+}
+vowels('AEIOU')
 module.exports = vowels;
