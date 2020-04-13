bf1afa76dd94d09fe7d3661b0060616e66914e5b
vaz007 <ankur@adaptideations.com>
2020-04-13 12:02:02 +1000

    Linked List Algo Ex

--- AlgoEx/linkedlist/index.js
+++ AlgoEx/linkedlist/index.js
@@ -2,8 +2,99 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+  size() {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      counter++;
+      node = node.next;
+    }
+    return counter;
+  }
+  getFirst() {
+    return this.head;
+  }
+  getLast() {
+    if (!this.head) {
+      return null;
+    } else {
+      let node = this.head;
+      while (node) {
+        if (!node.next) {
+          return node;
+        }
+        node = node.next;
+      }
+    }
+  }
 
+  clear() {
+    return (this.head = null);
+  }
+  removeFirst() {
+    if (this.head) {
+      return (this.head = this.head.next);
+    }
+    return;
+  }
+
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+    let previous = this.head;
+    let node = this.head.next;
+
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+    //  return previous;
+  }
+
+  insertLast(data) {
+    const last = this.getLast();
+    if (last) {
+      //Existing nodes
+      last.next = new Node(data);
+    } else {
+      // chain is empty
+
+      this.head = new Node(data);
+    }
+  }
+
+  getAt(index) {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+
+    }
+    return null;
+  }
+}
 module.exports = { Node, LinkedList };
